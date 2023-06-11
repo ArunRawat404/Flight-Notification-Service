c31222670cefa5d3ae5bca17087a932c883c4d26
arunrawat404 <arunrawatar404@gmail.com>
2023-06-11 23:32:32 +0530

    Added urlencoded and json parsing

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { InfoController } = require("../../controllers")
+const { InfoController } = require("../../controllers");
 
 const router = express.Router();
 
